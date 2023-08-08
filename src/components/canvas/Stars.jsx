6f3a74acd93b9a5687b29dd910abfbae81a95d2a
015591 <vmurab@questrade.com>
2023-08-07 23:12:18 -0400

    Added Stars Canvas,last commit for the day,will continue tommorow,PEACE && git push

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,12 +1,23 @@
-import React,{Suspense} from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
+import React,{Suspense,useState,useRef} from 'react'
+import { Canvas,useFrame } from '@react-three/fiber'
+import { Points,Preload,PointMaterial } from '@react-three/drei'
 import CanvasLoader from '../Loader'
+import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = () => {
+  const ref = useRef();
+  
   return (
     <div>Stars</div>
   )
 }
-
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-full absolute inset-0 z-[-1]  ">
+      <Canvas camera={{position :[0,0,1]}} />
+      <Suspense fallback = {null} > <Stars/> </Suspense>
+      <Preload all />
+    </div>
+  )
+}
 export default Stars
