988d23fcc688ec67e17ad15c418af6bc87b9d966
015591 <vmurab@questrade.com>
2023-08-07 23:03:51 -0400

    Added Planet in Contact Section(Email functionality not implemented yet)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { EarthCanvas } from "./canvas";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, slideIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+import StarWrapper from "../hoc/SectionWrapper";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+  return (
+    <div className="ml:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl "
+      >
+        <p className={styles.sectionSubText}>Get in Touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.value}
+              onChange={handleChange}
+              placeholder="Whats your name?"
+              className="bg-tertiary py-4 px-6  placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Whats your email?"
+              className="bg-tertiary py-4 px-6  placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+            rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6  placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl" type="submit" > {loading?'Sending...':'Send Message!'} </button>
+        </form>
+      </motion.div>
+      <motion.div className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] " variants={slideIn("right", "tween", 0.2, 1)} >
+        <EarthCanvas/>
+        
+      </motion.div>
+    </div>
+  );
+};
+
+export default StarWrapper(Contact, "contact");
