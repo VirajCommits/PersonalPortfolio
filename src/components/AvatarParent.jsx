2c37744bd7356a73dae456a4dd9afb83f0b318cf
015591 <vmurab@questrade.com>
2023-08-18 22:50:23 -0400

    Disabling OrbitControls on scrolling in mobile view

--- src/components/AvatarParent.jsx
+++ src/components/AvatarParent.jsx
@@ -1,13 +1,27 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Avatar } from "./Avatar";
 
 const AvatarParent = () => {
+  const cameraRef = useRef(); // Ref for camera
+  const [enableControls, setEnableControls] = useState(true); // State to enable/disable controls
+
+  const handleTouchStart = () => {
+    setEnableControls(false); // Disable controls when touch starts
+  };
+
+  const handleTouchEnd = () => {
+    setEnableControls(true); // Enable controls when touch ends
+  };
 
   return (
-    <Canvas shadows camera={{ position: [0, 2, 3], fov: 40 }}>
-      <OrbitControls enableZoom = {false} enablePan = {false} enableDamping = {false}  />
+    <Canvas
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
+      <perspectiveCamera ref={cameraRef} position={[0, 2, 3]} fov={40} />
+      <OrbitControls enableZoom={false} enabled={enableControls} />
       <group scale={[1, 1, 1]} position-y={-1.2}>
         <Avatar />
       </group>
