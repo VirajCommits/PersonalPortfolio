e9cf7582dc0b5b5c085d58749e9a16a216502698
015591 <vmurab@questrade.com>
2023-08-18 22:05:58 -0400

    changing desc

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,28 +17,42 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt options = {{
-        max:45,
-        scale:1,
-        speed:450,
-      }} className = "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full " >
-
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full "
+      >
         <div className="relative w-full h-230px">
-          <img className="w-full h-full object-cover rounded-2xl" src= {image} alt="" />
+          <img
+            className="w-full h-full object-cover rounded-2xl"
+            src={image}
+            alt=""
+          />
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover ">
-            <div onClick={() => window.open(source_code_link,"_blank")} className = "black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer " >
-              <img src= {github} alt="" className="w-1/2 h-1/2 object-contain" />
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer "
+            >
+              <img src={github} alt="" className="w-1/2 h-1/2 object-contain" />
             </div>
           </div>
         </div>
         <div className="mt-5  ">
-          <h3 className="text-white font-bold text-[24px]  " > {name} </h3>
-          {/* <p className="mt-2 text-secondary text-[14px]" > This is DescriptionThis is Description hello hello  hey</p> */}
+          <h3 className="text-white font-bold text-[24px]  "> {name} </h3>
+          <p className="mt-2 text-secondary text-[14px] overflow-hidden max-h-[40px] leading-[18px]">
+            {description}
+          </p>
         </div>
 
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
-            <p key={tag.name} className= {`text-[14px] ${tag.color} `} > #{tag.name} </p>
+            <p key={tag.name} className={`text-[14px] ${tag.color} `}>
+              {" "}
+              #{tag.name}{" "}
+            </p>
           ))}
         </div>
       </Tilt>
