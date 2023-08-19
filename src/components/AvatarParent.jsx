5a3d779b1db978dac6b3e244e43600f71c25f7b1
015591 <vmurab@questrade.com>
2023-08-19 08:18:32 -0400

    Disabling OrbitControls on scrolling in mobile view

--- src/components/AvatarParent.jsx
+++ src/components/AvatarParent.jsx
@@ -1,10 +1,9 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Avatar } from "./Avatar";
 
 const AvatarParent = () => {
-  const cameraRef = useRef(); // Ref for camera
   const [isTouching, setIsTouching] = useState(false); // State to track touch events
 
   const handleTouchStart = () => {
@@ -15,34 +14,13 @@ const AvatarParent = () => {
     setIsTouching(false);
   };
 
-  const handleWindowTouchStart = () => {
-    if (!isTouching) {
-      // Disable OrbitControls rotation and panning when touching the avatar
-      cameraRef.current.userData.controls.enabled = false;
-    }
-  };
-
-  const handleWindowTouchEnd = () => {
-    if (!isTouching) {
-      // Re-enable OrbitControls rotation and panning
-      cameraRef.current.userData.controls.enabled = true;
-    }
-  };
-
-  React.useEffect(() => {
-    window.addEventListener("touchstart", handleWindowTouchStart);
-    window.addEventListener("touchend", handleWindowTouchEnd);
-    return () => {
-      window.removeEventListener("touchstart", handleWindowTouchStart);
-      window.removeEventListener("touchend", handleWindowTouchEnd);
-    };
-  }, [isTouching]);
-
   return (
     <Canvas>
-      <perspectiveCamera ref={cameraRef} position={[0, 2, 3]} fov={40} userData={{ controls: null }}>
-        <OrbitControls ref={(controls) => (cameraRef.current.userData.controls = controls)} />
-      </perspectiveCamera>
+      <OrbitControls
+        enableZoom={false}
+        enableRotate={!isTouching}
+        enablePan={!isTouching}
+      />
       <group scale={[1, 1, 1]} position-y={-1.2}>
         <Avatar onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} />
       </group>
