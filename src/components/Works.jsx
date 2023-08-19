2f861543ee63b2766c72813d3476600ff221f151
015591 <vmurab@questrade.com>
2023-08-18 21:55:31 -0400

    changing desc

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -33,7 +33,7 @@ const ProjectCard = ({
         </div>
         <div className="mt-5  ">
           <h3 className="text-white font-bold text-[24px]  " > {name} </h3>
-          <p className="mt-2 text-secondary text-[14px]" > This is Description </p>
+          <p className="mt-2 text-secondary text-[14px]" > This is DescriptionThis is DescriptionThis is DescriptionThis is Description </p>
         </div>
 
         <div className="mt-4 flex flex-wrap gap-2">
