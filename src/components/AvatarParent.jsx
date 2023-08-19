03142981f545bc6978082b35f6ac1bf1dd089bec
015591 <vmurab@questrade.com>
2023-08-18 22:44:12 -0400

    trying to make scroll smooth

--- src/components/AvatarParent.jsx
+++ src/components/AvatarParent.jsx
@@ -1,42 +1,37 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Avatar } from "./Avatar";
 
 const AvatarParent = () => {
-  const controlsRef = useRef(); // Ref for OrbitControls instance
+  const cameraRef = useRef(); // Ref for camera
+  const [touchStartPosition, setTouchStartPosition] = useState(null);
 
   const handleTouchStart = (event) => {
-    const touch = event.touches[0];
-    const rect = touch.target.getBoundingClientRect();
-    const x = touch.clientX - rect.left;
-    const y = touch.clientY - rect.top;
+    setTouchStartPosition(event.touches[0].clientY);
+  };
 
-    // Calculate the dimensions and position of the avatar
-    const avatarLeft = 0; // Adjust these values to match your avatar's position
-    const avatarRight = 100;
-    const avatarTop = 0;
-    const avatarBottom = 100;
-
-    // Check if the touch event occurred over the avatar
-    if (x >= avatarLeft && x <= avatarRight && y >= avatarTop && y <= avatarBottom) {
-      // Disable camera controls while touch is over the avatar
-      if (controlsRef.current) {
-        controlsRef.current.enabled = false;
-      }
+  const handleTouchMove = (event) => {
+    if (touchStartPosition !== null) {
+      const deltaY = touchStartPosition - event.touches[0].clientY;
+      const currentPosition = cameraRef.current.position;
+      cameraRef.current.position.set(currentPosition.x, currentPosition.y + deltaY * 0.01, currentPosition.z);
+      setTouchStartPosition(event.touches[0].clientY);
     }
   };
 
   const handleTouchEnd = () => {
-    // Enable camera controls when touch ends
-    if (controlsRef.current) {
-      controlsRef.current.enabled = true;
-    }
+    setTouchStartPosition(null);
   };
 
   return (
-    <Canvas onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
-      <OrbitControls ref={controlsRef} enableZoom = {false} />
+    <Canvas
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <perspectiveCamera ref={cameraRef} position={[0, 2, 3]} fov={40} />
+      <OrbitControls enableZoom={false} />
       <group scale={[1, 1, 1]} position-y={-1.2}>
         <Avatar />
       </group>
